7213600772bb02bdc988b159c6700cb271108aeb
OksanaMosk <130758466+OksanaMosk@users.noreply.github.com>
2023-10-19 16:42:31 +0300

    3

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,5 +1,5 @@
 import { Component } from "react";
-import css from './ContactList.module.css';
+// import css from './ContactList.module.css';
 
 export default class ContactList extends Component {
     state = {
